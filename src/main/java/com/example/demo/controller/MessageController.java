f2f3501a833e1c9a90d6fc36367f69bbad877bad
tmvf <tmvf@yandex.ru>
2018-11-25 21:29:32 +0300

    02

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,77 +1,56 @@
 package com.example.demo.controller;
 
-import com.example.demo.exceptions.NotFoundException;
+import com.example.demo.domain.Message;
+import com.example.demo.domain.Views;
+import com.example.demo.repo.MessageRepo;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private int counter = 4;
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {
-        {
-            add(new HashMap<String, String>() {
-                {
-                    put("id", "1");
-                    put("text", "First message");
-                }
-            });
-            add(new HashMap<String, String>() {
-                {
-                    put("id", "2");
-                    put("text", "Second message");
-                }
-            });
-            add(new HashMap<String, String>() {
-                {
-                    put("id", "3");
-                    put("text", "Third message");
-                }
-            });
-        }
-    };
+
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageRepo.findAll();
     }
 
-    private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream()
-                .filter(message -> message.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
 
     @GetMapping("{id}")
-    public Map<String, String> getOne(@PathVariable String id) {
-        return getMessage(id);
-    }
-
-    @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-        messages.add(message);
+    @JsonView(Views.FullMessage.class)
+    public Message getOne(@PathVariable("id") Message message) {
         return message;
     }
 
-    @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id,
-                                      @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDB = getMessage(id);
-        messageFromDB.putAll(message);
-        messageFromDB.put("id", id);
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepo.save(message);
+    }
 
-        return messageFromDB;
+    @PutMapping("{id}")
+    public Message update(@PathVariable("id") Message messageFromDb,
+                          @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+
+        return messageRepo.save(messageFromDb);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) {
-        Map<String, String> messageFromDB = getMessage(id);
-        messages.remove(messageFromDB);
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,21 +1,106 @@
+function getIndex(list, id) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].id == id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 let messageApi = Vue.resource('/message{/id}');
 
-Vue.component("messages-row", {
-    props: ['message'],
-    template: `<div><i>{{message.id}}</i>. {{message.text}}</div>`
+Vue.component("messages-form", {
+    props: ['messages', 'messageAttr'],
+    data(){
+        return {
+            text: "",
+            id: ""
+        }
+    },
+    watch: {
+        messageAttr(newVal, oldVal){
+            this.text = newVal.text
+            this.id = newVal.id
+        }
+    },
+    template: `<div>
+    <input type="text" placeholder="Write text" v-model="text"/>
+    <input type="button" value="Save" @click="save"/>
+    </div>`
+    ,
+    methods: {
+        save(){
+            let message = {text: this.text}
+            if (this.id) {
+                messageApi.update({id: this.id}, message)
+                    .then(result =>
+                        result.json().then(data => {
+                            let index = getIndex(this.messages, data.id);
+                            this.messages.splice(index, 1, data);
+                            this.text = '';
+                            this.id = ''
+                        }));
+            } else {
+                messageApi.save({}, message)
+                    .then(result =>
+                        result.json().then(data => {
+                            this.messages.push(data)
+                        }));
+                this.text = '';
+            }
+        }
+    }
 });
+
+Vue.component("messages-row", {
+    props: ['message', 'editMethod','messages'],
+    template: `<div>
+<i>{{message.id}}</i>. {{message.text}}
+<span style="position: absolute; right: 0">
+<input type="button" value="Edit" @click="edit">
+<input type="button" value="X" @click="del">
+</span>
+</div>`
+    ,
+    methods: {
+        edit(){
+            this.editMethod(this.message)
+        },
+        del(){
+            messageApi.remove({id: this.message.id})
+                .then(result =>{
+                    if(result.ok){
+                        this.messages.splice(this.messages.indexOf(this.message),1)
+                    }
+                })
+        }
+    }
+});
+
 Vue.component("messages-list", {
     props: ['messages'],
-    template: `<div>
+    data(){
+        return {
+            message: null
+        }
+    },
+    template: `<div style="position: relative; width: 300px">
+<messages-form :messages="messages" :messageAttr="message" ></messages-form>
 <messages-row v-for="message in messages" 
-:key="message.id" :message="message"></messages-row>
+:key="message.id" :message="message" :messages="messages"
+:editMethod="editMethod"></messages-row>
 </div>`,
-    created:function () {
+    created: function () {
         messageApi.get()
             .then(result =>
-            result.json()
-                .then(data =>
-               data.forEach(message => this.messages.push(message))))
+                result.json()
+                    .then(data =>
+                        data.forEach(message => this.messages.push(message))))
+    },
+    methods: {
+        editMethod(message){
+            this.message = message;
+        }
     }
 });
 
