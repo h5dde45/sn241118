d55104c14df268309b1305196983327e8ab910d6
tmvf <tmvf@yandex.ru>
2018-11-27 18:38:16 +0300

    04

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div v-if="!profile">Авторизация <a href="/login">Google</a></div>
+        <div v-else>
+            <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
+            <message-list :messages="messages"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MessageList from "components/messages/MessageList.vue"
+    import {addHandler} from "util/ws"
+    import {getIndex} from "util/collections"
+    export default{
+        components: {
+            MessageList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile,
+            }
+        },
+        create(){
+            addHandler(data => {
+                let index = getIndex(this.messages, data.id);
+                if (index > -1) {
+                    this.messages.splice(index, 1)
+                } else {
+                    this.messages.push(data)
+                }
+            })
+        }
+    }
+</script>
+
+<style>
+</style>
